b4dbbe5786328f6ef8f3bdfae031f881ab26580f
antleypk <peterantley@gmail.com>
2019-09-15 23:48:10 -0400

    fixed strip punctuation test

--- application/unit_tests.py
+++ application/unit_tests.py
@@ -8,17 +8,17 @@ class Test(unittest.TestCase):
     
     def test_strip_punctuation(self):
         normal = 'hello, mr jackson.?!@#'
-        after = ['h','e','l','l','o','',' ','m','r',' ','j','a','c','k','s','o','n','','','','','']
+        after = ['h', 'e', 'l', 'l', 'o', '', '', 'm', 'r', '', 'j', 'a', 'c', 'k', 's', 'o', 'n', '', '', '', '', '']
         self.assertEqual(analyzer.strip_punctuation(normal), after)
 
     
     def test_get_recent_run(self):
-        self.assertEqual(analyzer.get_recent_run('./tests/'),'1568572639')
+        self.assertEqual(analyzer.get_recent_run('./test_data/'),'1568572639')
 
     def test_get_shops(self):
         key = scraper.get_key()
         shops = [{'count': '1', 'id': '21162039', 'name': 'SmartSVGDesigns', 'listings': '121', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '2', 'id': '21159771', 'name': 'Roseobelle', 'listings': '32', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '3', 'id': '21158587', 'name': 'GregorySvg', 'listings': '28', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '4', 'id': '21155263', 'name': 'CathlenBalonArtSVG', 'listings': '28', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '5', 'id': '21155083', 'name': 'DELLAJewelryDesigns', 'listings': '34', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '6', 'id': '21155035', 'name': 'LittleGoblinGarments', 'listings': '27', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '7', 'id': '21154863', 'name': 'HeArtsandDesign', 'listings': '30', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '8', 'id': '21153881', 'name': 'BUNGKUSROKOK', 'listings': '27', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '9', 'id': '21152823', 'name': 'TOEBROOTWORLD', 'listings': '29', 'key': 'bvpvd0ns8aqk63229f9baz9u'},{'count': '10', 'id': '21152773', 'name': 'ArtesianHandcraft', 'listings': '58', 'key': 'bvpvd0ns8aqk63229f9baz9u'}]
-        self.assertEqual(analyzer.get_shops('1568572639',key,'./tests/'), shops)
+        self.assertEqual(analyzer.get_shops('1568572639',key,'./test_data/'), shops)
 
     def test_get_unique_words(self):
         lcl_set = set()
